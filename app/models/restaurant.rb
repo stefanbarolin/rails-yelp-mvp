896200d334ec3569769a6a80aec2626cd1b3cfdb
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-09-29 20:35:16 +0200

    Change in Model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
   validates :category, in: ["chinese", "italian", "japanese", "french", "belgian"]
-  has_many: :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end
