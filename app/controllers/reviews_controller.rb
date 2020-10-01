a0c02da4d7fd16ef12073fc73b59a677696ea8e7
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-01 20:43:09 +0200

    Review new ok

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(set_params)
+    if @review.save
+      redirect_to new_restaurant_review_path(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_params
+    params.require(@review).permit(:content, :rating, :restaurant_id)
+  end
 end
