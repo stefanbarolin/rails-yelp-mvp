4cf0466f8e72c756791f07bef6372e033030cd00
Stefan BAROLIN <stefan.barolin@gmail.com>
2020-10-03 12:02:54 +0200

    Need to implement the reviews into Restaurant page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,16 +6,20 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(set_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to new_restaurant_review_path(@review.restaurant)
     else
       render :new
     end
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
 
   def set_params
-    params.require(@review).permit(:content, :rating, :restaurant_id)
+    params.require(:review).permit(:content, :rating)
   end
 end
